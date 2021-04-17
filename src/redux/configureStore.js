79eda6f1000ca5dfa48f57fef60ac519a16da8a7
ishuen <12228644+ishuen@users.noreply.github.com>
2021-04-17 23:43:49 +0800

    edit readme, remove unused package and enhancer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-
-import monitorReducerEnhancer from './monitorReducerEnhancer';
 import reducer from './reducer';
 
 export default function configureStore(preloadedState) {
   const middlewares = [thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
-  const enhancers = [middlewareEnhancer, monitorReducerEnhancer];
+  const enhancers = [middlewareEnhancer];
   const composedEnhancers = compose(...enhancers);
 
   const store = createStore(reducer, preloadedState, composedEnhancers);
